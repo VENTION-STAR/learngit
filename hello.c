0ac1387505f0685f03ff1d47fb5dbcea2ec044ac
m39k <1437920338@qq.com>
2025-02-04 20:03:34 +0800

    在新分支dev的修改

--- hello.c
+++ hello.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
 int add(int a, int b);
+int chen(int a, int b);
+
 int main()
 {
     printf("hello world");
 
     return 0 ;
 }
+
+
+
 int add(int a, int b)
 {	return a+b ;
+}
+int chen(int a, int b)
+{
+	return a*b ;
 }
--- readme.txt
+++ readme.txt
@@ -1,3 +1,4 @@
 git is a distributed version control system
 git is a free software
 challo ``
+i creating a new branch 
